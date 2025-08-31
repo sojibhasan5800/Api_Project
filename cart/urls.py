f28acdd14eb39ca684df35190909e1e922619474
phitron-demo <sojibhasan5800@gmail.com>
2025-08-31 09:02:56 +0600

    feat(cart): implement cart API endpoints for listing, adding, removing, and updating items

--- cart/urls.py
+++ cart/urls.py
@@ -1,9 +1,14 @@
-from django.urls import path,include
+from django.urls import path
+from .views import CartViewSet
 
-
+cart_list = CartViewSet.as_view({'get': 'list'})
+add_item = CartViewSet.as_view({'post': 'add_item'})
+remove_item = CartViewSet.as_view({'delete': 'remove_item'})
+update_quantity = CartViewSet.as_view({'patch': 'update_quantity'})
 
 urlpatterns = [
-    
+    path('cart_api/', cart_list, name='cart-detail'),
+    path('cart/add_api/', add_item, name='cart-add-item'),
+    path('cart/remove/<int:pk>/', remove_item, name='cart-remove-item'),
+    path('cart/update/<int:pk>/', update_quantity, name='cart-update-quantity'),
 ]
-
-
