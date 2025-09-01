13080fc1f214f86fe5543ac7464e0e35f68a011f
phitron-demo <sojibhasan5800@gmail.com>
2025-09-01 10:46:44 +0600

    Enhance Category admin panel with search, filter, slug prepopulation and image preview

--- categories/admin.py
+++ categories/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Category
+from django.utils.html import format_html
 
-# Register your models here.
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('category_name', 'slug', 'url', 'cat_image_tag')
+    prepopulated_fields = {'slug': ('category_name',)}
+    search_fields = ('category_name', 'description')
+    list_filter = ('category_name',)
+    readonly_fields = ('cat_image_tag',)
+
+    def cat_image_tag(self, obj):
+        if obj.cat_image:
+            return format_html('<img src="{}" style="height:50px;width:50px;border-radius:5px;" />', obj.cat_image.url)
+        return "-"
+    cat_image_tag.short_description = 'Category Image'
